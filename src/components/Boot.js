d6f17ff72df12c07af14d342c0bf7549f7081a62
Seth Battin <sbattin@truecar.com>
2017-01-17 21:11:28 -0600

    results from react-boot-example experiments

--- src/components/Boot.js
+++ src/components/Boot.js
@@ -5,41 +5,39 @@ class Boot extends Component {
   render() {
     return null;
   }
+  static bodyScripts = [];
 }
-
-Boot.propTypes = {
-  script: PropTypes.object
-};
 
 function reducePropsToState(propsList) {
-  console.log("reducePropsToState", propsList);
-  const tags = {};
+  const bodyScripts = Boot.bodyScripts;
   propsList.forEach(props => {
-    const tagName = Object.keys(props).slice(-1).pop();
-    Object.assign(tags, {[tagName]: props[tagName]});
+    let {children} = props;
+    if (! children.hasOwnProperty('length')){
+      children = [children];
+    }
+    children.forEach(child => {
+      if (child.type == "script" && bodyScripts.indexOf(child.props.children ) === -1){
+        bodyScripts.push(child.props.children);
+      }
+    });
   });
-  //console.log(tags);
-  return tags;
+  return bodyScripts;
 }
 
-function handleStateChangeOnClient(elementTypes) {
-  console.log("handleStateChangeOnClient", elementTypes);
+function handleStateChangeOnClient(bodyScripts) {
+  console.log("handleStateChangeOnClient", bodyScripts);
   let bootElement = document.getElementById("gluestick-boot");
 
   if (!bootElement) {
     bootElement = document.createElement("div");
     bootElement.id = "gluestick-boot";
     document.body.appendChild(bootElement);
-
-    Object.keys(elementTypes).forEach((element) => {
-      const domElement = document.createElement(element);
-      domElement.type = elementTypes[element].type;
-      domElement.innerHTML = elementTypes[element].innerHTML;
-      console.log("appending child:", domElement);
-      bootElement.appendChild(domElement);
-    });
+    bodyScripts.forEach((script) => {
+      const el = document.createElement('script');
+      el.appendChild(document.createTextNode(script));
+      bootElement.appendChild(el);
+    })
   }
-
 }
 
 export default withSideEffect(
